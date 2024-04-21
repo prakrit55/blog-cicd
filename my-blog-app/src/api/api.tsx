8e766cabc3aaef1402e425c5310ae2e49d87939e
Griffin <prakritimandal611@gmail.com>
2024-04-21 17:18:48 +0530

    added changes
    Signed-off-by: Griffin <prakritimandal611@gmail.com>

--- my-blog-app/src/api/api.tsx
+++ my-blog-app/src/api/api.tsx
@@ -1,24 +1,30 @@
 // api.ts
 import axios from 'axios';
 
-const API_URL = 'cc-demo-app-alb-1963131864.ap-south-1.elb.amazonaws.com';
+const API_URL = 'http://cc-demo-app-alb-670050430.ap-south-1.elb.amazonaws.com/api';
 
 export const fetchPosts = async () => {
-  const response = await axios.get<any[]>(`${API_URL}/api/posts`);
-  return response.data;
+  try {
+    const response = await axios.get<any[]>(`${API_URL}/posts`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    throw error; // Optionally rethrow the error to handle it in the calling function
+  }
 };
 
+
 export const fetchPost = async (postId: string | any) => {
-  const response = await axios.get<any>(`${API_URL}/api/posts/${postId}`);
+  const response = await axios.get<any>(`${API_URL}/posts/${postId}`);
   return response.data; // Ensure that response.data contains the expected post data
 };
 
 export const createPost = async (postData: any) => {
-  const response = await axios.post<any>(`${API_URL}/api/posts`, postData);
+  const response = await axios.post<any>(`${API_URL}/posts`, postData);
   return response.data;
 };
 
 export const deletePost = async (postId: string | any) => {
-  const response = await axios.delete<any>(`${API_URL}/api/posts/${postId}`);
+  const response = await axios.delete<any>(`${API_URL}/posts/${postId}`);
   return response.data;
 };
